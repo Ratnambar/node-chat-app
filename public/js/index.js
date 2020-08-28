eaca2265764e1dfbcee64865a74bc95e64907eb3
Ratnambar <ratnambar123gupta@gmail.com>
2020-08-28 16:10:55 +0530

    display form messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,34 @@
 var socket = io();
 
-socket.on('connect',()=>{
-	console.log('Conneted to the server.');
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//   from: 'Frank',
+//   text: 'Hi'
+// });
+
+jQuery('#message-form').on('submit',function(e){
+	e.preventDefault();
 
 	socket.emit('createMessage',{
-		from:"ratnambar gupta",
-		text:"knit sultanpur"
+		from:'User',
+		text:jQuery('[name=message]').val()
+	}, function(){
+
 	});
-
-
 });
 
-socket.on('newMsg',function(data){
-	console.log(data);
 
-});
-
-socket.emit('reply',{
-		"text":"got message."
-	});
-
-socket.on('newMessage',function(msg){
-	console.log('New Message',msg);
-});
-
-socket.on('disconnect',()=>{
-	console.log('Disconnected from the server.');
-});
--- server/server.js
+++ server/server.js
@@ -1,54 +1,40 @@
-const path=require('path');
-var http = require('http');
+const path = require('path');
+const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
 
-const publicPath=path.join(__dirname,'../public');
-var port = process.env.PORT|| 3000;
-var app=express();
+const {generateMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
 var server = http.createServer(app);
-var io=socketIO(server);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-console.log(__dirname + '/../public');
-console.log(publicPath);
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
-io.on('connection',(socket)=>{
-	console.log('new user is connected.');
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-	socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app.'));
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    // callback('This is from the server.')
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
-
-	socket.broadcast.emit('newMessage',generateMessage('Admin','New user has joined.'));
-
-
-	socket.on('createMessage',(message)=>{
-		io.emit('newMessage',generateMessage(message.from,message.text));
-	});
-
-
-	  
-
-	socket.emit('newMsg',{
-		"from":"ratnambar",
-		"text":"hello everyone."
-
-	});
-
-
-
-	socket.on('reply', function(data){
-		console.log(data);
-	});
-	
-
-socket.on('disconnect',()=>{
-	console.log('Disconnected from the server.');
-});
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
-server.listen(port,()=>{
-	console.log(`Server is listening at ${port}.`);
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
