28129d5cd5792a910f553fb71edf146a049b726d
Ratnambar <ratnambar123gupta@gmail.com>
2020-08-28 19:11:09 +0530

    send location with username

--- public/js/index.js
+++ public/js/index.js
@@ -15,10 +15,16 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
-// socket.emit('createMessage', {
-//   from: 'Frank',
-//   text: 'Hi'
-// });
+socket.on('newLocationMessage',function(message){
+	var li = jQuery('<li></li>');
+	var a = jQuery('<a href="_blank">My Current Location</a>');
+
+	li.text(`${message.from}: `);
+	a.attr('href',message.url);
+	li.append(a);
+	jQuery('#messages').append(li);
+});
+
 
 jQuery('#message-form').on('submit',function(e){
 	e.preventDefault();
@@ -31,4 +37,18 @@ jQuery('#message-form').on('submit',function(e){
 	});
 });
 
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(){
+	if(!navigator.geolocation){
+		return alert("Geolocation is not supported by your browser.");
+	}
+	navigator.geolocation.getCurrentPosition(function(position){
+		socket.emit('createLocationMessage',{
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+	},function(){
+		alert('Unable to fetch loaction.');
+	});
+});
 
--- server/utils/message.js
+++ server/utils/message.js
@@ -5,4 +5,12 @@ var generateMessage = (from, text)=>{
 	};
 };
 
-module.exports = {generateMessage};
+
+var generateLocationMessage = (from,latitude,longitude)=>{
+	return {
+		from,
+		url:`https://www.google.com/maps?q=${latitude},${longitude}`,
+	};
+};
+
+module.exports = {generateMessage,generateLocationMessage};
